a21a9b1a99b9e9fea492665bb3828d00bbd77ca2
Init project <congpgph26764@fpt.edu.vn>
2023-09-27 22:12:30 +0700

    first commit

---
+++ app/src/main/java/com/hn_2452/shoes_nike/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hn_2452.shoes_nike
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
